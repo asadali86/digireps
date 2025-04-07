075551e8e9e51cd6207872d3f79bdb011c0f8b32
asadali86 <aaliaali86@gmail.com>
2025-04-07 19:51:17 +0500

    two files coming

--- src/app/sections/GrowCostEffectively.tsx
+++ src/app/sections/GrowCostEffectively.tsx
@@ -6,7 +6,7 @@ export const GrowCostEffectively = () => {
     <section className='growCostEffectively pl-25 pr-25 pb-25'>
         <div className="blueBox flex items-center p-25">
             <div className="leftCol">
-                <h3 className='text-center rounded-full dot-heading font-medium mb-5 px-10 py-4'><span className='pl-2'>Let's Meet</span></h3>
+                <h3 className='text-center rounded-full dot-heading font-medium mb-5 px-10 py-4'><span className='pl-2'>Lets Meet</span></h3>
                 <h2 className='text-white font-medium mb-5'>want to learn more how we can help you grow cost-effectively?</h2>
                 <button className='bgColorBlue text-white rounded-full px-15 py-4'>Book Discovery session</button>
             </div>
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
         <div className='flex'>
             <div className='col-01 flex flex-col items-center justify-center'>
-                <img src="/images/digireps-logo-sm.webp" alt="DigiReps" className='mb-5' />
+                <Image src="/images/digireps-logo-sm.webp" alt=" " width={191} height={62} className='mb-5' />
                 <p className='text-center'>The smart way to grow your team <br /> remotely!</p>
             </div>
             <div className='col-02'>
@@ -20,9 +20,9 @@ const Footer = () => {
             <div className='col-03 p-8'>
                 <h3 className='text-white mb-5'>Highly Rated</h3>
                 <div className="flex items-center mb-5">
-                    <img src="/images/google-reviews-logo.webp" alt="Google" />
-                    <img src="/images/trust-pilot-logo.webp" alt="Trustpilot" />
-                    <img src="/images/clutch-logo.webp" alt="Clutch" />
+                    <Image src="/images/google-reviews-logo.webp" alt=" " width={121} height={61} />
+                    <Image src="/images/trust-pilot-logo.webp" alt=" " width={128} height={61} />
+                    <Image src="/images/clutch-logo.webp" alt=" " width={155} height={44} />
                 </div>
                 <h3 className='text-white mb-5'>Get in Touch</h3>
                 <p className='mb-5'>447 Broadway, Ste 7168 New York - 10013, United States</p>
